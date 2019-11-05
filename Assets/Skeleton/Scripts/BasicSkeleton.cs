5506b4cb2d0f9f469427b479ad16ba938fcaad29
David Foo <davidfoo98@gmail.com>
2019-11-05 10:38:50 +0000

    Local commit before pull

---
+++ Assets/Skeleton/Scripts/BasicSkeleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSkeleton : MonoBehaviour
+{
+    JobManager jobManager; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jobManager = GameObject.Find("JobManager").GetComponent<JobManager>();
+        if(jobManager == null)
+        {
+            Debug.Log("NO JOB MANAGER FOUND");
+        }
+        else
+        {
+            Debug.Log("JobManager testString: " + jobManager.testString);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
