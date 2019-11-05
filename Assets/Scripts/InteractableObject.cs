d2f6f4f15795481a6aa8baee4d4cb6e72421901f
Sam Flint <legomansam@gmail.com>
2019-11-05 12:04:35 +0000

    Added base interactable object script

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    protected virtual void OnMouseDown()
+    {
+        int test = 0;
+    }
+
+    void OnMouseEnter()
+    {
+        //when the mouse moves over the object, give it a outline
+    }
+}
