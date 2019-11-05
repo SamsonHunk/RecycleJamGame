563b71a6a00980ea1bc9085775ce69e0373fb901
Sam Flint <legomansam@gmail.com>
2019-11-05 11:42:59 +0000

    Finalised Camera Controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,42 +5,51 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     Camera camera;
+    Vector3 offset;
     public GameObject focus;
     public float speed = 10;
-    Vector3 initial;
+    bool locked = true;
 
     // Start is called before the first frame update
     void Start()
     {
         camera = GetComponent<Camera>();
-        initial = camera.transform.localPosition;
         Cursor.lockState = CursorLockMode.Confined;
+        offset = camera.transform.position - focus.transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.mousePosition.y >= Screen.height * .95f)
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed, focus.transform);
+    {//move the camera according to the mouse input if the camera is not locked to the player
+        if (locked)
+        {//if we are locked set the transform of the camera to the focus object's transform
+            camera.transform.position = focus.transform.position + offset;
         }
-        else if (Input.mousePosition.y <= Screen.height * .05f)
+        else
         {
-            transform.Translate(Vector3.back * Time.deltaTime * speed, focus.transform);
+            if (Input.mousePosition.y >= Screen.height * .95f)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed, focus.transform);
+            }
+            else if (Input.mousePosition.y <= Screen.height * .05f)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * speed, focus.transform);
+            }
+
+            if (Input.mousePosition.x >= Screen.width * .95f)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed, focus.transform);
+            }
+            else if (Input.mousePosition.x <= Screen.width * .05f)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed, focus.transform);
+            }
         }
 
-        if (Input.mousePosition.x >= Screen.width * .95f)
+        //if space is pressed lock the camera to the player
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speed, focus.transform);
-        }
-        else if (Input.mousePosition.x <= Screen.width * .05f)
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * speed, focus.transform);
-        }
-
-        if (Input.GetAxis("Refocus") > 0)
-        {
-            camera.transform.localPosition = initial; 
+            locked = !locked;
         }
     }
 }
