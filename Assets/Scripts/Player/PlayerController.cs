fe758dfb42ba985abbfcbc5bb9b6a15c687291e4
Sam Flint <legomansam@gmail.com>
2019-11-06 14:30:57 +0000

    Added player model

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     Vector3 moveVector;
     public float speed = 1;
     public bool working { get; set; }
+    float rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,24 @@ public class PlayerController : MonoBehaviour
         {
             moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveVector *= speed;
+            if (Input.GetKey(KeyCode.W))
+            {
+                rotation = 0;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rotation = 90;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rotation = 180;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                rotation = 270;
+            }
 
+            this.transform.localRotation = Quaternion.Euler(new Vector3(0, rotation, 0));
             controller.Move(moveVector * Time.deltaTime);
         }
     }
