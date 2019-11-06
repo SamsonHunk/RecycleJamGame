98498815ee35438758252ba0f99bc74034c4db5d
David Foo <davidfoo98@gmail.com>
2019-11-06 12:18:26 +0000

    Added minion status UI and fixed pixelation

---
+++ Assets/Scripts/UI Script/MinionContextScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinionContextScript : MonoBehaviour
+{
+    Text textbox;
+    string outputText;
+    Minion self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = gameObject.GetComponentInChildren<Canvas>().GetComponentInChildren<Text>();
+        if(textbox == null)
+        {
+            Debug.Log("Could not get textbox");
+        }
+        self = gameObject.GetComponent<Minion>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        outputText = self.currentCommand.ToString();
+        if(self.carryAmount != 0)
+        {
+            outputText += "\n ";
+            ouputText += "Bones: "
+            outputText += self.carryAmount.ToString();
+        }
+        textbox.text = outputText;
+    }
+}
