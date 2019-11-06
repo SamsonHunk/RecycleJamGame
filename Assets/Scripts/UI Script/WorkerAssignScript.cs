1be8c117aeba48354ee3c9f249a8b3f3026b04b1
David Foo <davidfoo98@gmail.com>
2019-11-06 14:47:16 +0000

    Added menu art, script and buttons

---
+++ Assets/Scripts/UI Script/WorkerAssignScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkerAssignScript : MonoBehaviour
+{
+    Text textbox;
+    int numberWorkers = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string outputText = "Assign ";
+        outputText += numberWorkers.ToString();
+        outputText += "\n";
+        if(numberWorkers == 1)
+        {
+            outputText += "Worker";
+        }
+        else
+        {
+            outputText += "Workers";
+        }
+        textbox.text = outputText;
+    }
+
+    public void IncreaseWorkers()
+    {
+        ++numberWorkers;
+        if(numberWorkers > 3)
+        {
+            numberWorkers = 3;
+        }
+    }
+    public void DecreaseWorkers()
+    {
+        --numberWorkers;
+        if (numberWorkers < 1)
+        {
+            numberWorkers = 1;
+        }
+    }
+}
